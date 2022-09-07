dc90d255497221c4b35a6488a54afee042c42b42
Shubbham Desai <desai99150@gmail.com>
2022-09-07 11:36:51 +0530

    added port variable

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-const express = require('express')
-const productRouter = require('./routes/productRoutes')
-const cors = require('cors')
-const app = express()
-const dotenv = require('dotenv');
+const express = require("express");
+const productRouter = require("./routes/productRoutes");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
 
-const mongoose = require('mongoose');
-const path = require('path')
-dotenv.config({ path: './config.env' });
-
+const mongoose = require("mongoose");
+const path = require("path");
+dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
-  '<password>',
+  "<password>",
   process.env.DATABASE_PASSWORD
 );
-mongoose
-  .connect(DB)
-  .then((con) => {
-    // console.log(con.connections)
-    console.log('DB connection successful');
-  });
+mongoose.connect(DB).then((con) => {
+  // console.log(con.connections)
+  console.log("DB connection successful");
+});
 
-
-
-
-
-app.use(cors())
-app.use('/api/v1/products', productRouter)
-
-app.listen(8000, ()=> {
-    console.log('server is running on port 8000')
-})
+app.use(cors());
+app.use("/api/v1/products", productRouter);
+const port = 8000 || process.env.PORT;
+app.listen(port, () => {
+  console.log("server is running on port 8000");
+});
